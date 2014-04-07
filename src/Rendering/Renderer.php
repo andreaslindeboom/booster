ec79259fd6cdea305e78e0e598d886f4dab50cf8
Andreas Lindeboom <andreas@deinternetjongens.nl>
2014-04-07 22:22:36 +0200

    Removed faulty URL in file header

--- src/Rendering/Renderer.php
+++ src/Rendering/Renderer.php
@@ -5,7 +5,7 @@
  * @author     Andreas Lindeboom
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  * @copyright  2014 Andreas Lindeboom
- * @link       http://github.com/andreaslindeboom/minion
+ * @link       http://github.com/andreaslindeboom
  */
 namespace Rendering;
 
