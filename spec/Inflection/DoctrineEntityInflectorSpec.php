c212b38a90d740e895390fe043c26694d4d9fbb1
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2014-04-13 11:35:09 +0200

    Entity inflector spec

--- spec/Inflection/DoctrineEntityInflectorSpec.php
+++ spec/Inflection/DoctrineEntityInflectorSpec.php
@@ -4,11 +4,44 @@ namespace spec\Inflection;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use spec\Data\ConfigSpecData;
 
-class DoctrineInflectorSpec extends ObjectBehavior
+class DoctrineEntityInflectorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Inflection\DoctrineEntityInflector');
+    }
+
+    function it_should_inflect_a_space_separated_entity()
+    {
+        $entity = "prospective customer";
+
+        $this->generateInflections($entity)
+            ->shouldReturn(ConfigSpecData::getSimpleInflections());
+    }
+
+    function it_should_inflect_a_camel_cased_entity()
+    {
+        $entity = "prospectiveCustomer";
+
+        $this->generateInflections($entity)
+            ->shouldReturn(ConfigSpecData::getSimpleInflections());
+    }
+
+    function it_should_inflect_a_snake_cased_entity()
+    {
+        $entity = "prospective_customer";
+
+        $this->generateInflections($entity)
+            ->shouldReturn(ConfigSpecData::getSimpleInflections());
+    }
+
+    function it_should_inflect_a_studly_cased_entity()
+    {
+        $entity = "ProspectiveCustomer";
+
+        $this->generateInflections($entity)
+            ->shouldReturn(ConfigSpecData::getSimpleInflections());
     }
 }
